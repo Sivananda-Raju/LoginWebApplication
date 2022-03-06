187afdac76e85d997e58797fb8f5736711df635f
Sivnaanda raju <sivananda_raju@dell.com>
2022-03-06 12:48:52 +0530

    Checkin with AngularLogin

--- LoginWebApp/ClientApp/login-app/src/app/authservice.service.ts
+++ LoginWebApp/ClientApp/login-app/src/app/authservice.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { baseUrl } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class AuthserviceService {
   constructor(private http: HttpClient) { }
   login(data: any): Observable<any> {
     console.log(data);
-    return this.http.post(`${baseUrl}authenticate`, data);
+    return this.http.post(`http://localhost:60044/api/authenticate`, data);
   }
 
 
